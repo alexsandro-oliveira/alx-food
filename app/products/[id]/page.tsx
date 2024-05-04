54b9cdea013914a2f731f2d641df791d7d91f31a
Alexsandro Santos <ale.osantos@hotmail.com>
2024-05-04 15:43:24 +0100

    feat: add product page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,48 @@
+import { db } from "@/app/_lib/prisma";
+import { notFound } from "next/navigation";
+import ProductDetails from "./_components/product-details";
+import ProductImge from "./_components/produtc-image";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const ProductPage = async ({ params: { id } }: ProductPageProps) => {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      {/* IMAGEM */}
+      <ProductImge product={product} />
+
+      {/* TITULO E PREÇO */}
+      <ProductDetails product={product} complementaryProducts={juices} />
+    </div>
+  );
+};
+
+export default ProductPage;
