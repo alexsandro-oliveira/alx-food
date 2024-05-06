23a4b9f2cad3f22257f4a8085fb9aa086f68ec12
Alexsandro Santos <ale.osantos@hotmail.com>
2024-05-06 02:25:47 +0100

    refactor: move link inside button

--- app/page.tsx
+++ app/page.tsx
@@ -50,12 +50,16 @@ const Home = async () => {
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
+
           <Button
             variant="ghost"
             className="h-fit p-0 text-xs text-primary hover:bg-transparent"
+            asChild
           >
-            Ver todos
-            <ChevronRightIcon size={16} />
+            <Link href="/products/recommended">
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Link>
           </Button>
         </div>
 
@@ -73,15 +77,16 @@ const Home = async () => {
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Restaurantes Recomendados</h2>
 
-          <Link href="/restaurants/recommended">
-            <Button
-              variant="ghost"
-              className="h-fit p-0 text-xs text-primary hover:bg-transparent"
-            >
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-xs text-primary hover:bg-transparent"
+            asChild
+          >
+            <Link href="/restaurants/recommended">
               Ver todos
               <ChevronRightIcon size={16} />
-            </Button>
-          </Link>
+            </Link>
+          </Button>
         </div>
 
         <RestaurantList />
